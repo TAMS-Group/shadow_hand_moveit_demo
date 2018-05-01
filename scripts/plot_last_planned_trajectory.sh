1d72abc56689d81f22e33fadf23696da5f4d531b
v4hn <me@v4hn.de>
2018-05-01 21:07:54 +0800

    plot trajectory profile in one window

--- scripts/plot_last_planned_trajectory.sh
+++ scripts/plot_last_planned_trajectory.sh
@@ -7,6 +7,6 @@ rostopic echo -n1 /move_group/display_planned_path > "$F"
 cat "$F" | sed -n 's/.*positions: \[//; T; s/, /,/g; T; s/\]$//g; T; p' > "$F.pos"
 cat "$F" | sed -n 's/.*velocities: \[//; T; s/, /,/g; T; s/\]$//g; T; p' > "$F.vel"
 
-octave-cli --eval "P=csvread('$F.pos'); V=csvread('$F.vel'); plot(P); figure; plot(V); waitforbuttonpress"
+octave-cli --eval "P=csvread('$F.pos'); V=csvread('$F.vel'); subplot(2,1,1); plot(P); subplot(2,1,2); plot(V); waitforbuttonpress"
 
 rm "$F" "$F.pos" "$F.vel"
